13dc25baf8c9eafb0f48ae3d6a0bdef5c74d123c
alexander.buryakov@gmail.com <alexander.buryakov@gmail.com@97ffabc6-d9e2-0a36-87c9-c9199cc0fd63>
2012-01-28 21:31:38 +0000

    added hover event to table

--- trunk/scripts/test.js
+++ trunk/scripts/test.js
@@ -1,1 +1,33 @@
 //window.onload = function(){ alert("welcome"); }
+
+function initNumTable(table) {
+
+    $(table + ' td').each(function (i, el) {
+
+        $(this).hover(
+            function () {
+                $(this).animate({"opacity":"1"}, { queue:true, duration:350 });
+            },
+            function () {
+                $(this).animate({"opacity":".2"}, { queue:true, duration:350 });
+            }
+        );
+
+        $(this).click(function () {
+            $(this).hide("slow");
+        });
+    })
+
+
+    $("a").click(function (event) {
+//        alert("Thanks for visiting!");
+        event.preventDefault();
+        $(this).hide("slow");
+
+    });
+
+}
+$(document).ready(function () {
+    initNumTable('.number_table');
+
+});
