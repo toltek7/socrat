e2f8a502dfa2cb6ad00ca140b9097e252640e371
alexander.buryakov@gmail.com <alexander.buryakov@gmail.com@97ffabc6-d9e2-0a36-87c9-c9199cc0fd63>
2012-01-30 20:57:19 +0000

    add numbers and js functions (ajax)

--- trunk/scripts/test.js
+++ trunk/scripts/test.js
@@ -2,15 +2,32 @@
 
 var cellX = 10;
 var cellY = 10;
-
-
-function readNumbersValues(){
-
-
+var codesNumbers ={
+    numb : new Array(),
+    word : new Array()
+}
+//Note: 101 - max numbers of cell in html table
+var cellsMaxNum = 101;
+var startNumMin = 0;
+var startNumMax = 49;
+var randomArray = new Array(cellsMaxNum);
+function setRandomArray(min, max){
+    for(var i = 0; i< cellsMaxNum; i++){
+        randomArray[i] = Math.floor(Math.random()*(max - min + 1) + min);
+    }
 }
 
+function readNumbersValues(xml){
 
-function buildNumTable(container, table, x, y) {
+    $(xml).find("num").each(function()
+    {
+        codesNumbers.numb.push($(this).attr("value"));
+        codesNumbers.word.push($(this).text());
+    });
+    setRandomArray(startNumMin,startNumMax);
+}
+
+function buildNumTableHtml(container, table, x, y) {
     var $tbl = $(table).empty();
     var $tr = $('<tr>');
 
@@ -29,17 +46,13 @@ function initNumTable(table) {
 
     $(table + ' td').each(function (i, el) {
 
-
         var  $cellNum = $('<span>').attr("class","num");
         var  $cellWord = $('<span>').attr("class","word");
 
         var $div = $('<div>');
-        $div.append($cellWord.text("ГаГарин"));
-
-        $div.append($cellNum.text(11));
-
+        $div.append($cellWord.text(codesNumbers.word[randomArray[randomArray[i]]]));
+        $div.append($cellNum.text(codesNumbers.numb[randomArray[randomArray[i]]]));
         $(this).append($div);
-
 
         $(this).hover(
             function () {
@@ -49,7 +62,7 @@ function initNumTable(table) {
             },
             function () {
                 if($(this).hasClass("opened"))
-                    $(this).stop().animate({"opacity":".7"}, { queue:true, duration:350 });
+                    $(this).stop().animate({"opacity":".8"}, { queue:true, duration:350 });
                 else
                     $(this).stop().animate({"opacity":".5"}, { queue:true, duration:350 });
             }
@@ -66,12 +79,20 @@ function initNumTable(table) {
 
 }
 
-
-
 $(document).ready(function () {
 
-    readNumbersValues();
-    buildNumTable('.wdt_1200','.number_table',10,5);
+    //http://www.switchonthecode.com/tutorials/xml-parsing-with-jquery
+    $.ajax({
+        type: "GET",
+        url: '../data/numbers.xml',
+        async: false,
+        dataType: "xml",
+        isLocal: true,
+        success: readNumbersValues
+    });
+
+
+    buildNumTableHtml('.wdt_1200','.number_table',8,6);
     initNumTable('.number_table');
 
 //    $("#slider").slider();
